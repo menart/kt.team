c26438b5cc3305531d6263bc6240f3d18ee30e92
Aleksandr Artamonov <artamonov@mail.tvil.ru>
2023-01-30 20:31:31 +0500

    feat: add main page

---
+++ back/src/Controller/MainController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(path: '/')]
+class MainController extends AbstractController
+{
+    #[Route(path: '', methods: ['GET'])]
+    public function index(Request $request): Response
+    {
+        $list = [
+            [
+                'href' => '/products',
+                'title' => 'Список товаров',
+            ],
+            [
+                'href' => '/import',
+                'title' => 'Импорт товаров',
+            ],
+
+        ];
+        return $this->render('index.twig', [
+           'title' => 'main',
+            'list' => $list,
+        ]);
+    }
+}
