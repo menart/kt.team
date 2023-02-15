698efef3581e6d7aa3e7f81966ff510a445b177a
Aleksandr Artamonov <artamonov@mail.tvil.ru>
2023-02-16 00:33:21 +0500

    feat:add test case for MigrationEventSubscriber

---
+++ back/tests/unit/Symfony/MigrationEventSubscriberTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace UnitTests\Symfony;
+
+use App\Symfony\MigrationEventSubscriber;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+class MigrationEventSubscriberTest extends TestCase
+{
+    private EventDispatcher $dispatcher;
+
+    public function setUp(): void
+    {
+        $this->dispatcher = new EventDispatcher();
+    }
+
+    public function testPostGenerateSchema()
+    {
+        $listener = new MigrationEventSubscriber();
+        $this->assertEquals(['postGenerateSchema'], $listener->getSubscribedEvents());
+        $this->dispatcher->addListener('postGenerateSchema', [$listener, 'postGenerateSchema']);
+
+        // dispatch your event here
+        $entitytManager = \Mockery::mock(EntityManager::class);
+        $schema = new Schema();
+
+        $args = new GenerateSchemaEventArgs($entitytManager, $schema);
+        $this->dispatcher->dispatch($args, 'postGenerateSchema');
+        $schema = $args->getSchema();
+        $this->assertTrue($schema->hasNamespace('public'));
+    }
+}
